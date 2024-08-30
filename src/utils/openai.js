bda1518c09664507efaa0b315a71dd45522d0c78
Ashish Yadav <aybhu10@gmail.com>
2024-08-30 21:36:46 +0530

    updates openAI apis

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,9 +1,9 @@
-import OpenAI from 'openai';
-import { OPENAI_KEY } from './constants';
+// import OpenAI from 'openai';
+// import { OPENAI_KEY } from './constants';
 
-const client = new OpenAI({
-  apiKey: OPENAI_KEY,
-  dangerouslyAllowBrowser: true
-});
+// const client = new OpenAI({
+//   apiKey: OPENAI_KEY,
+//   dangerouslyAllowBrowser: true
+// });
 
-export default client;
+// export default client;
