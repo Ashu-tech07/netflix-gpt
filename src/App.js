ff20ca26aab5795098e653d231a73f89d01c2766
Ashish Yadav <aybhu10@gmail.com>
2024-11-10 15:07:40 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux';
 import Body from './components/Body';
 import appStore from './redux/appStore';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <Provider store={appStore}>
       <Body />
+      <Footer />
     </Provider>
   );
 }
